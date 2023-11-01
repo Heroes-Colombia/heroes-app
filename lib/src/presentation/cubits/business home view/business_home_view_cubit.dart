59c50d2a1843a1bdfda573fb09e98846c2e55835
Andres <andres@deadline.com.mx>
2023-11-01 16:57:03 -0600

    Removed: unused package

--- lib/src/presentation/cubits/business home view/business_home_view_cubit.dart
+++ lib/src/presentation/cubits/business home view/business_home_view_cubit.dart
@@ -6,7 +6,6 @@ import 'package:get_it/get_it.dart';
 import 'package:heroes_app/assets/app_constants.dart';
 import 'package:heroes_app/assets/app_enums.dart';
 import 'package:heroes_app/src/domain/models/listable_business_model.dart';
-import 'package:heroes_app/src/domain/repositories/firestorage_service.dart';
 import 'package:heroes_app/src/domain/repositories/firestore_service.dart';
 
 part 'business_home_view_state.dart';
