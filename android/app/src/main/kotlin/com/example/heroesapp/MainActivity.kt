9ae68b258eeceb9535526b4920c0dc00dae9f30d
Andres Ch <andres@deadline.com.mx>
2024-01-02 00:11:47 -0600

    Added: Env files for swift ui native code

---
+++ android/app/src/main/kotlin/com/example/heroesapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.heroesapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-     GMSServices.provideAPIKey("AIzaSyB9CAK0XhZdzdXjpb9jo6R-XvT7IKSagSM")
+      GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["AIzaSyB9CAK0XhZdzdXjpb9jo6R-XvT7IKSagSM"])
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
